f41835518bbd0d31192f5b9f5e83a899bce36846
MatheuAlves <matheu98@hotmail.com>
2025-02-07 10:24:56 -0300

    ADD SVM and KNN

--- main_dataset_diabetes.py
+++ main_dataset_diabetes.py
@@ -5,33 +5,33 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-# Carregar o dataset (substitua ’seu_dataset .csv’ pelo arquivo baixado )
+# Carregar o dataset
 df = pd.read_csv("dataset_diabetes.csv")
 
-# Inspecionar os dados
+print('DATASET:\n')
 print(df.head())
 
 # Pré-processar os dados (ajuste as colunas conforme necessário)
-X = df [['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']] #Insira as colunas de entrada
-y = df ['Outcome'] #Insira a coluna alvo
+X = df [['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']]
+y = df ['Outcome']  # Coluna alvo
 
 # Dividir os dados em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test =  train_test_split (X , y , test_size=0.3 , random_state =42)
 
 # Criar e treinar a árvore de decisão
-clf = DecisionTreeClassifier(max_depth=3, random_state=21)
-clf.fit(X_train, y_train)
+clf_tree = DecisionTreeClassifier(max_depth=3, random_state=21)
+clf_tree.fit(X_train, y_train)
 
 # Fazer previsões e avaliar
-y_pred = clf.predict(X_test)
-print(f"Acurácia: {accuracy_score(y_test, y_pred):.2f}")
+y_pred_tree = clf_tree.predict(X_test)
+print(f"Acurácia (DECISION TREE): {accuracy_score(y_test, y_pred_tree):.2f}")
 
 # Visualizar a árvore de decisão
 plt.figure(figsize=(40, 55))
 plot_tree(
-    clf,
+    clf_tree,
     feature_names=X.columns,
-    class_names=[str(cls) for cls in clf.classes_], 
+    class_names=[str(cls) for cls in clf_tree.classes_], 
     filled=True,
     rounded=True,
     fontsize=10
@@ -45,3 +45,21 @@ plt.legend(handles=handles, loc='best', fontsize=12)
 
 plt.title("Árvore de Decisão para Classificação de Diabetes", fontsize=18)
 plt.show()
+
+
+
+# KNN
+from sklearn . neighbors import KNeighborsClassifier
+
+clf_knn = KNeighborsClassifier (n_neighbors=5)
+clf_knn . fit (X_train ,y_train )
+y_pred_knn = clf_knn . predict ( X_test )
+print ( f"Acurácia (KNN): {accuracy_score (y_test , y_pred_knn ):.2f}")
+
+# SVM
+from sklearn . svm import SVC
+
+clf_svm = SVC ( kernel ='linear', random_state =42)
+clf_svm.fit (X_train, y_train )
+y_pred_svm = clf_svm.predict ( X_test )
+print(f"Acurácia (SVM): {accuracy_score(y_test, y_pred_svm ):.2f}")
